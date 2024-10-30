4ec5dfefc6e84e1e416613fcc638062437116844
Maya Eid <mayabaseleid@gmail.com>
2024-10-30 21:57:34 +0200

    Add content to win screen and set car as interactive

--- scenes/winScene.js
+++ scenes/winScene.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import WebFont from "webfontloader";
 
 export default class Win extends Phaser.Scene{
 
@@ -6,7 +7,32 @@ export default class Win extends Phaser.Scene{
         super('win-screen')
     }
 
+    init(data) {
+        //get the selected car
+        this.selectedCar= data.car; 
+        console.log(this.selectedCar);
+
+    }
+
     create(){
-        console.log("In win screen");   
+        this.add.text(600, 100, 'You reached the finish line!', { 
+            fontFamily: '"Press Start 2P"',
+            fontSize: '32px', 
+            color: '#fff' 
+        }).setOrigin(0.5);
+
+        this.add.text(600, 200, 'Click on your car to go to the next level', { 
+            fontFamily: '"Press Start 2P"',
+            fontSize: '16px', 
+            color: '#fff' 
+        }).setOrigin(0.5);
+
+        this.car = this.physics.add.sprite(600,400,this.selectedCar)  
+       .setScale(3);
+
+        this.car.setInteractive();
+        this.car.on('pointerdown', () => {
+            this.scene.start('moderate-screen',{car:this.selectedCar});
+        });
     }
 }
